6a1925bcf8728d1c1fdaf1c4010139dd80a02288
rajendra kumar <rajendrakumar2770@gmail.com>
2022-01-28 22:50:48 +0530

    removed unnecessary folder

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -34,6 +34,7 @@ class SceneMain extends Phaser.Scene{
         // this.cameras.main.startFollow(this.player, true, 0.8, 0.8);
         this.cursor = this.input.keyboard.createCursorKeys();
         this.input.on('pointerdown', this.shoot, this);
+        this.gas = this.physics.add.image(this.factory.x, this.factory.y, 'gas').setScale(0.2).setOrigin(0, 0.5);
     }
     shoot()
     {
