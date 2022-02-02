5ddbccc2249744b6c5f8ac84ad1d4422c5283a6b
raj2770 <cs19b034@iittp.ac.in>
2022-02-02 20:19:56 +0530

    bomb size decreased

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -62,7 +62,7 @@ class SceneMain extends Phaser.Scene{
     shoot()
     {
         if(this.activateBrahmos){
-            this.laser = this.physics.add.image(this.player.x, this.player.y, 'brahmos').setScale(0.1).setOrigin(0, 0.5);
+            this.laser = this.physics.add.image(this.player.x, this.player.y, 'brahmos').setScale(0.15).setOrigin(0, 0.5);
 
             this.physics.moveTo(this.laser, this.game.input.mousePointer.x, this.game.input.mousePointer.y, 600);
             this.physics.add.collider(this.laser, this.gas, this.destroyGas, null, this);
