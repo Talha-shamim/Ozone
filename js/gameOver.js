d2a52499c697d276dfaf3c25146a95332c5214dd
raj2770 <cs19b034@iittp.ac.in>
2022-02-02 19:16:06 +0530

    game over text modfied

--- js/gameOver.js
+++ js/gameOver.js
@@ -18,9 +18,9 @@ class GameOver extends Phaser.Scene{
         this.player = this.physics.add.image(300,266,"player").setScale(0.7);
 
         // Use player.state.username to get player's username
-        this.gameOverText = this.add.text(550,266, `${player.state.username} Game over`, {fontSize : '72px', fill : '#fff'})
+        this.gameOverText = this.add.text(550,266, `${player.state.username} game over`, {fontSize : '72px', fill : '#fff', fontFamily : 'Arial',FontStyle : 'bold', align : 'center', stroke : '#000', strokeThickness : 5, wordWrap : true, wordWrapWidth : 800 ,textTransform : 'uppercase'});
 
-        this.score = this.add.text(630,330, 'Your score '+ this.point, {fontSize : '30px', fill : '#fff'})
+        this.score = this.add.text(630,330, 'Your score '+ this.point, {fontSize : '30px', fill : '#fff', fontFamily : 'Arial', padding : 10});
 
         this.startBtn = this.add.sprite(750, 400, 'restart').setInteractive().setScale(0.4);
 
